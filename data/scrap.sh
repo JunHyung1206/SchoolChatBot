720423336c620af2c702f045a7b065f891793e19
JunHyung1206 <pjh120610@naver.com>
2024-03-31 14:22:52 +0900

    Fix: 쉘 스크립트 수정

--- data/scrap.sh
+++ data/scrap.sh
@@ -1,7 +1,7 @@
 python main.py \
--l 'https://www.kumoh.ac.kr/ko/sub06_01_01_03.do' \
+-l 'https://www.kumoh.ac.kr/ko/sub06_01_01_01.do' \
 --num_workers 4
 
 python main.py \
--l 'https://www.kumoh.ac.kr/ko/sub06_01_01_01.do' \
+-l 'https://www.kumoh.ac.kr/ko/sub06_01_01_03.do' \
 --num_workers 4
