d5518df640a3929d655c0deacf75d49de1d4c8bd
JunHyung1206 <pjh120610@gmail.com>
2024-04-08 09:57:06 +0900

    Style: autopep8 포매터 적용

--- data/noticescraper.py
+++ data/noticescraper.py
@@ -11,6 +11,7 @@ from utils import remove_html_tags, html_table_to_markdown
 
 MAX_RETRIES = 3
 
+
 class NoticeScraper:
     def __init__(self, args):
         self.args = args
@@ -20,10 +21,11 @@ class NoticeScraper:
         self.df = None
 
         conf = OmegaConf.load("api_key.yaml")
-        self.imgProcessor = ImageProcessor(conf["API_SECRET_KEY"], conf["OCR_URL"])        
-        
+        self.imgProcessor = ImageProcessor(
+            conf["API_SECRET_KEY"], conf["OCR_URL"])
+
         if not os.path.exists('datasets'):
-            os.mkdir('datasets')    
+            os.mkdir('datasets')
         if not os.path.exists('cache'):
             os.mkdir('cache')
         if not os.path.exists('docs'):
@@ -37,21 +39,25 @@ class NoticeScraper:
             except Exception as e:
                 if retry == MAX_RETRIES - 1:
                     raise e
-                print(f"An error occurred during scraping: {e}. Retrying... (Retry {retry + 1}/{MAX_RETRIES})")
+                print(
+                    f"An error occurred during scraping: {e}. Retrying... (Retry {retry + 1}/{MAX_RETRIES})")
 
         file_name = self.base_url.split('/')[-1].split('.')[0]
-        self.df.to_csv(f'./datasets/{file_name}.csv', encoding='utf8', index=False)
-        
+        self.df.to_csv(f'./datasets/{file_name}.csv',
+                       encoding='utf8', index=False)
 
     def _scraping_routine(self):
         file_name = self.base_url.split('/')[-1].split('.')[0]
         if os.path.exists(f'./cache/{file_name}_cache.csv'):
-            self.df = pd.read_csv(f'./cache/{file_name}_cache.csv', encoding='utf8')
+            self.df = pd.read_csv(
+                f'./cache/{file_name}_cache.csv', encoding='utf8')
         else:
-            self.df = pd.DataFrame(columns=['title', 'category', 'date', 'url', 'content'])
+            self.df = pd.DataFrame(
+                columns=['title', 'category', 'date', 'url', 'content'])
 
         total_page_step = self.page_step * self.num_workers
-        page_num = max(len(self.df) - 10, 0) // total_page_step * total_page_step
+        page_num = max(len(self.df) - 10,
+                       0) // total_page_step * total_page_step
 
         if page_num > 0:
             self.df = self.df[:page_num + 10]  # 겹치는 항목 제외
@@ -60,7 +66,8 @@ class NoticeScraper:
             print(page_num)
             try:
                 with Pool(4) as pool:
-                    singleThread_dfs = pool.map(self.single_thread_scraping, [page_num + i * self.page_step for i in range(self.num_workers)])
+                    singleThread_dfs = pool.map(self.single_thread_scraping, [
+                                                page_num + i * self.page_step for i in range(self.num_workers)])
                     pool.close()
                     pool.join()
 
@@ -68,7 +75,8 @@ class NoticeScraper:
                 print(f"An error occurred during multi-threading: {e}")
                 raise e
 
-            step_df = pd.DataFrame(columns=['title', 'category', 'date', 'url', 'content'])
+            step_df = pd.DataFrame(
+                columns=['title', 'category', 'date', 'url', 'content'])
             for sdf in singleThread_dfs:
                 step_df = pd.concat([step_df, sdf])
 
@@ -78,7 +86,8 @@ class NoticeScraper:
             page_num += total_page_step
             self.df = pd.concat([self.df, step_df])
             self.df.reset_index(inplace=True, drop=True)
-            self.df.to_csv(f'./cache/{file_name}_cache.csv', encoding='utf8', index=False)
+            self.df.to_csv(
+                f'./cache/{file_name}_cache.csv', encoding='utf8', index=False)
         self.df.reset_index(inplace=True, drop=True)
 
     def single_thread_scraping(self, page_num):
@@ -88,16 +97,20 @@ class NoticeScraper:
             except Exception as e:
                 if retry == MAX_RETRIES - 1:
                     raise e
-                print(f"An error occurred during single thread scraping: {e}. Retrying... (Retry {retry + 1}/{MAX_RETRIES})")
+                print(
+                    f"An error occurred during single thread scraping: {e}. Retrying... (Retry {retry + 1}/{MAX_RETRIES})")
 
     def _single_thread_scraping_routine(self, page_num):
-        list_url = self.base_url + f'?mode=list&&articleLimit={self.page_step}&article.offset={page_num}'
+        list_url = self.base_url + \
+            f'?mode=list&&articleLimit={self.page_step}&article.offset={page_num}'
         response = requests.get(list_url)
 
         if response.status_code == 500:
-            raise Exception(f"500 Internal Server Error for {list_url}. Maximum retries exceeded.")
+            raise Exception(
+                f"500 Internal Server Error for {list_url}. Maximum retries exceeded.")
         elif response.status_code != 200:
-            raise Exception(f"Failed to fetch the page {list_url}. Status code: {response.status_code}")
+            raise Exception(
+                f"Failed to fetch the page {list_url}. Status code: {response.status_code}")
 
         page_index = 10 if page_num != 0 else 0
 
@@ -118,17 +131,17 @@ class NoticeScraper:
             url_list[idx] = self.base_url + title.attrs['href']
 
         temp_df = pd.DataFrame({
-            'title':title_list,
-            'category':category_list,
-            'date' : date_list,
-            'url' : url_list
+            'title': title_list,
+            'category': category_list,
+            'date': date_list,
+            'url': url_list
         })
 
         content_list = []
         for url in temp_df['url']:
             content = self.page_scraping(url)
             content_list.append(content)
-            
+
         temp_df['content'] = content_list
         return temp_df
 
@@ -137,7 +150,7 @@ class NoticeScraper:
         soup = BeautifulSoup(response.content, 'html.parser')
         if self.args.docs:
             self.get_docs(soup)
-        
+
         try:
             result = self.get_content(soup)
         except Exception as e:
@@ -145,7 +158,7 @@ class NoticeScraper:
             print(page_url)
             return ""
         return result
-    
+
     def get_content(self, soup):
         content = soup.select(".board-contents")[0]
         tables = content.select(".board-contents table")
@@ -154,26 +167,26 @@ class NoticeScraper:
 
         imgs = content.select(".board-contents img")
         content = str(content)
-            
+
         for table in tables:
             markdown_table = html_table_to_markdown(table)
             s1 = re.search('<table', content)
             s2 = re.search('</table>', content)
-            content = content.replace(content[s1.start():s2.end()], markdown_table)
+            content = content.replace(
+                content[s1.start():s2.end()], markdown_table)
 
         for img in imgs:
             image_url = img.attrs['src']
             if re.search('cms/', image_url):
-                if not re.search('https://www.kumoh.ac.kr/',image_url):
+                if not re.search('https://www.kumoh.ac.kr/', image_url):
                     image_url = 'https://www.kumoh.ac.kr/'+img.attrs['src']
-            
+
             s1 = re.search('<img[^>]+>', content)
             ocrText = self.imgProcessor.image_to_content(image_url)
             content = content.replace(content[s1.start():s1.end()], ocrText)
 
         content_text = remove_html_tags(content)
         return content_text
-    
 
     def get_docs(self, soup):
         docs = soup.select("a.file-down-btn")
@@ -182,5 +195,3 @@ class NoticeScraper:
             file_name = f'./docs/{doc.getText().strip()}'
             if not os.path.exists(file_name):
                 wget.download(file_url, out=file_name)
-
-
--- data/utils.py
+++ data/utils.py
@@ -1,10 +1,6 @@
-import os
 import re
-import requests
-import wget
-import pandas as pd
 from bs4 import BeautifulSoup
-from omegaconf import OmegaConf
+
 
 def remove_html_tags(html_text):
     # p 태그 처리
@@ -15,7 +11,8 @@ def remove_html_tags(html_text):
     html_text = re.sub(r'<br\s*/?\s*>', '\n', html_text)
 
     # 나머지 태그 처리
-    content_text = BeautifulSoup(html_text, 'html.parser').getText(separator=" ")
+    content_text = BeautifulSoup(
+        html_text, 'html.parser').getText(separator=" ")
 
     # 이후 후처리
     content_text = re.sub(r'\xa0+', ' ', content_text)
@@ -24,6 +21,7 @@ def remove_html_tags(html_text):
     content_text = re.sub(r'\ +', ' ', content_text).strip()
 
     return content_text
+
 
 def html_table_to_markdown(table):
     rows = table.find_all('tr')
