5d33db0dc9824d9b9294238667eb1aa85b18d00f
JunHyung1206 <pjh120610@gmail.com>
2024-04-11 09:50:28 +0900

    Fix: 딕셔너리 수정

--- data/main.py
+++ data/main.py
@@ -2,20 +2,10 @@ import argparse
 from scraper import NoticeScraper, FAQScraper, GuideScraper
 
 path_Scarper = {
-    "sub02_03_02_01": "FAQScraper",
-    "sub02_03_02_02": "FAQScraper",
-    "sub02_03_02_03": "FAQScraper",
-    "sub02_03_02_04": "FAQScraper",
-    "sub02_03_03_01": "GuideScraper",
-    "sub02_03_03_02": "GuideScraper",
-    "sub02_03_03_03": "GuideScraper",
-    "sub02_03_03_04": "GuideScraper",
-    "sub02_03_03_05": "GuideScraper",
-    "sub02_03_03_06": "GuideScraper",
-    "sub02_03_03_07": "GuideScraper",
-    "sub02_03_03_08": "GuideScraper",
-    "sub06_01_01_01": "NoticeScraper",
-    "sub06_01_01_03": "NoticeScraper",
+    "sub02_03_02": "FAQScraper",
+    "sub02_03_03": "GuideScraper",
+    "sub02_03_04": "GuideScraper",
+    "sub06_01_01": "NoticeScraper",
 }
 
 if __name__ == '__main__':
@@ -31,6 +21,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     file_name = args.url.split('/')[-1].split('.')[0]
-
-    s = globals()[path_Scarper[file_name]](args)
+    category = '_'.join(file_name.split('_')[0:-1])
+    s = globals()[path_Scarper[category]](args)
     s.scraping()
--- data/scrap.sh
+++ data/scrap.sh
@@ -1,3 +1,12 @@
+# python main.py \
+#     -l https://www.kumoh.ac.kr/ko/sub06_01_01_01.do \
+#     --num_workers 4
+
+# python main.py \
+#     -l https://www.kumoh.ac.kr/ko/sub06_01_01_03.do \
+#     --num_workers 4
+
+
 # for((i=1;i<5;i++))
 # do
 #     python main.py \
@@ -13,10 +22,10 @@
 # done
 
 
-python main.py \
-    -l https://www.kumoh.ac.kr/ko/sub06_01_01_01.do \
-    --num_workers 4
 
-python main.py \
-    -l https://www.kumoh.ac.kr/ko/sub06_01_01_03.do \
+for((i=1;i<5;i++))
+do
+    python main.py \
+    -l https://www.kumoh.ac.kr/ko/sub02_03_04_0$i.do \
     --num_workers 4
+done
